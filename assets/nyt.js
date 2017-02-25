ff31db5cb710e7d066c13181c593f59ac8a0691b
marco alvarado <Marco@marcos-MacBook-Pro.local>
2017-02-25 13:53:56 -0800

    records request works now

--- assets/nyt.js
+++ assets/nyt.js
@@ -1,14 +1,28 @@
 $("#searchButton").on("click", function(event){
 	event.preventDefault();
 	var term= $("#searchTerm").val().trim();
-	var records= $("#recordNumber").val();
+	var recordsRequested= $("#recordNumber").val();
 	var startYear=$("#start").val().trim().toString();
 	var endYear=$("#end").val().trim().toString();
 	var word= encodeURI(term);
 
+	if(startYear===""){
+		startYear="20000101";
+	}
+	else{
+		startYear+="0101";
+	}
+
+	if(endYear===""){
+		startYear="20171231";
+	}
+	else{
+		endYear+="1231";
+	}
+
 	var authKey= "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
 	var queryURL= "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
- authKey + "&q="+word+"&begin_date="+startYear+"0101"+"&end_date="+endYear+"1231"+"&fp="+records+"?hl=true";
+ authKey + "&q="+word+"&begin_date="+startYear+"&end_date="+endYear+"&hl=true";
 
  $.ajax({
  	url: queryURL,
@@ -16,7 +30,7 @@ $("#searchButton").on("click", function(event){
  }).done(function(responseNY) {
  	var results= responseNY.response.docs;
  	$("#resultsHere").empty();
- 	for(i=0; i<results.length; i++){
+ 	for(i=0; i<recordsRequested; i++){
  		var articleDiv= $("<div>");
  		var title= $("<h2>").text(results[i].headline.main);
  		var section=$("<p>").text("Section: "+results[i].section_name);
